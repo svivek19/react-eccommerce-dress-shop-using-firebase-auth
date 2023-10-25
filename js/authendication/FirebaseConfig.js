fe39fd587bae6c76f5ef8bcce8bd84058fe91c90
Vivekananthan S <129610457+svivek19@users.noreply.github.com>
2023-10-25 20:03:56 +0530

    some changes

--- js/authendication/FirebaseConfig.js
+++ js/authendication/FirebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDD_t89Lm3c4PrSuiKbEVO6uVt6yM_-HF8",
+  apiKey: "AIzaSyDD_t89Lm3c4PrSuriKbEVO6uVt6yM_-HF8",
   authDomain: "react-ecommerce-vivek.firebaseapp.com",
   projectId: "react-ecommerce-vivek",
   storageBucket: "react-ecommerce-vivek.appspot.com",
   messagingSenderId: "1040354117107",
-  appId: "1:1040354117107:web:819fb369d8ba7688a97e86",
+  appId: "1:1040354117107:web:819fb3we69d8ba7688a97e86",
 };
 
 const app = initializeApp(firebaseConfig);
